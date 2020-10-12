f1bf957f0636e100ad334455cd523cdbb106d066
Souvik <souvik.9401@gmail.com>
2020-10-12 18:33:45 +0530

    server application committed

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+var name;
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('joining msg', (username) => {
+  	name = username;
+  	io.emit('chat message', `---${name} joined the chat---`);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('chat message', `---${name} left the chat---`);
+    
+  });
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on :3000');
+});
+
+
